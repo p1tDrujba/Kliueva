b7da71819065a21e6f5aab3af7feae29b6cfa9b4
p1tDrujba <p.borshchev@gmail.com>
2020-03-22 22:31:15 +0300

    Сделан есь сайт для десктопа
    Без интерактива

---
+++ js/jq.js
@@ -0,0 +1,37 @@
+// $(document).on('click','.menu-btn', function(e) {
+//     e.preventDefault;
+//     $(this).toggleClass('menu-btn_active');
+//     $('.mob-menu__bot').toggleClass('mob-menu__bot_active')
+// });
+
+
+$(document).ready(function(){
+    
+    $('.summary-page').click(function() {
+        $(this).addClass('active-page');
+        $('.about-page').removeClass('active-page')
+        $('.portfolio-page').removeClass('active-page')
+        $('.about').removeClass('show')
+        $('.portfolio').removeClass('show')
+        $('.summary').addClass('show')
+    })
+
+    $('.about-page').click(function() {
+        $(this).addClass('active-page');
+        $('.summary-page').removeClass('active-page')
+        $('.portfolio-page').removeClass('active-page')
+        $('.summary').removeClass('show')
+        $('.portfolio').removeClass('show')
+        $('.about').addClass('show')
+    })
+
+    $('.portfolio-page').click(function() {
+        $(this).addClass('active-page');
+        $('.summary-page').removeClass('active-page')
+        $('.about-page').removeClass('active-page')
+        $('.summary').removeClass('show')
+        $('.about').removeClass('show')
+        $('.portfolio').addClass('show')
+    })
+
+});
